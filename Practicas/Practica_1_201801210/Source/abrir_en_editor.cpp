07ceb742eb8be0219f5664fc71a876f96273b9ab
AlexanderAvalos <60333367+AlexanderAvalos@users.noreply.github.com>
2020-02-14 23:43:41 -0600

    Update abrir_en_editor.cpp

--- Practicas/Practica_1_201801210/Source/abrir_en_editor.cpp
+++ Practicas/Practica_1_201801210/Source/abrir_en_editor.cpp
@@ -4,37 +4,74 @@
 #include <ncurses.h>
 #include <curses.h>
 #include <fstream>
+#include <string>
+#include <iomanip>
+#include <string.h>
+#include <stdio.h>
 using namespace std;
+
 
 WINDOW *nueva_ventana_E(int largo, int ancho, int pos_y, int pos_x);
 bool cont = false;
 string nombre;
 void reconocer_Nombre(string dir);
 void lectura(string rut);
- int x,y;
+int x,y;
+
 
 void Abrir_En_Editor::crear_EditorA(){
     WINDOW *ventana;
     char buscar;
     int marcox=75,marcoy=20;
-    char cadena;
+    char cadena,car;
+    string direccion;
+    int contador=0,canti;
     string ruta;
     initscr();
-    raw();
     refresh();
     ventana = nueva_ventana_E(marcoy, marcox, 2,2);
-    keypad(ventana,true);
     wprintw(ventana,"^W buscar y editar  ^C Reportes  ^S Guardar");
-    wmove(ventana,marcoy-2,4);
-    wprintw(ventana,"Ingresar Ruta :");
-    cadena = wgetch(ventana);
-   while(cadena != 10){
+    wmove(ventana,marcoy-2,2);
+    wprintw(ventana,"Ingresar Ruta: ");
+   do{
         cadena = wgetch(ventana);
-        ruta = ruta + cadena;
+            ruta = ruta + cadena;
+    }while(cadena != 10);
+    canti = ruta.length();
+    while(contador!=canti){
+        if(contador ==0){
+            direccion = direccion+'/';
+        }else{
+        direccion = direccion + ruta[contador+1];}
+        contador++;
     }
-    cout<< ruta <<endl;
-    reconocer_Nombre(ruta);
+    reconocer_Nombre(direccion);
     wmove(ventana,2,2);
+    string texto;
+    ifstream archivo;
+    char prue;
+    if(!archivo.is_open()){
+        archivo.open(direccion,ios::in);
+        if(archivo.fail()){
+            wmove(ventana,2,2);
+            cout<<direccion<<endl;
+            cout<<texto.length();
+            wprintw(ventana,"error al leer archivo");
+        }else{
+            while (!archivo.eof()) {
+                getline(archivo,texto);
+            }
+
+        }
+        archivo.close();
+        for(int i = 0; i <= texto.length();i++){
+            prue = texto[i];
+            wprintw(ventana,"%c",prue);
+        }
+    }
+
+    keypad(ventana,true);
+    raw();
     int caracter;
     while(cont != true){
         getyx(ventana,y,x);
@@ -106,8 +143,7 @@ void Abrir_En_Editor::crear_EditorA(){
 }
 
 void reconocer_Nombre(string dir){
-    lectura(dir);
-    int contador,contador2;
+    int contador = 0,contador2=0;
     bool cona = false;
     int cantidad = dir.length();
     while(cona != true){
@@ -123,30 +159,12 @@ void reconocer_Nombre(string dir){
         }
         if(contador == cantidad){
             cona = true;
+            cout<< nombre;
         }
         contador++;
     }
 }
-
-
-void lectura(string rut){
-    string texto;
-    ifstream Archivo;
-    Archivo.open(rut,ios::in);
-    if (Archivo.fail()){
-        cout<<"ERROR"<<endl;
-        exit(1);
-    }
-    while (!Archivo.eof()){
-            getline(Archivo,texto);
-            cout<<texto;
-    }
-
-    Archivo.close();
-}
-
-
-
+//home/alex/Escritorio/prueba2.txt
 
 
 WINDOW *nueva_ventana_E(int largo, int ancho, int pos_y, int pos_x)
