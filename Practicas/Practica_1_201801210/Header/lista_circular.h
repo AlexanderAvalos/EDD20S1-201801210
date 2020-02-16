01815280a9af97145ebee84cd87982ff997290d2
AlexanderAvalos <60333367+AlexanderAvalos@users.noreply.github.com>
2020-02-15 23:49:52 -0600

    Update lista_circular.h

--- Practicas/Practica_1_201801210/Header/lista_circular.h
+++ Practicas/Practica_1_201801210/Header/lista_circular.h
@@ -1,14 +1,54 @@
-#ifndef LISTA_CIRCULAR_H
-#define LISTA_CIRCULAR_H
+#include "datos_ruta.h"
+#include <stdlib.h>
+#include <iostream>
+#include <stdio.h>
+using namespace std;
 
-
-class lista_circular
+class NodoC
 {
+private:
+    Rutas *rutas;
 public:
-    lista_circular();
-    static void mostrar_Lista();
-    static void insertar_NodoC(char data);
-    static bool vacio();
-};
+  Rutas *primeroC;
+  Rutas *ultimoC;
 
-#endif // LISTA_CIRCULAR_H
+  bool vacio(){
+      if(primeroC == NULL){
+         return true;
+      }
+      return false;
+  }
+
+  void insertar_NodoC(string nombre,string direccion){
+      Rutas *nuevo = new Rutas();
+      nuevo->setnombre(nombre);
+      nuevo->setdireccion(direccion);
+      if(vacio()==true){
+          primeroC = nuevo;
+          nuevo->setsiguiente(ultimoC);
+          ultimoC = primeroC;
+      }else{
+          ultimoC->setsiguiente(nuevo);
+          nuevo->setsiguiente(primeroC);
+          ultimoC = nuevo;
+      }
+  }
+
+  void imprimir(){
+      Rutas *aux = new Rutas();
+          aux=primeroC;
+          int i=1;
+
+          if(primeroC!=NULL)
+           {
+                do
+                {    cout<<"  "<<aux->getnombre();
+                    cout<<" "<<aux->getdireccion();
+                     aux = aux->getsiguiente();
+                     i++;
+                }while(aux!=primeroC);
+            }
+           else
+               cout<<"\n\n\tLista vacia...!"<<endl;
+  }
+};
