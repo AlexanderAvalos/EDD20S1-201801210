8000fb9e58e76fa5b0ce9dc8d6f96d2e234e10bc
AlexanderAvalos <60333367+AlexanderAvalos@users.noreply.github.com>
2020-02-15 23:47:36 -0600

    Update abrir_en_editor.h

--- Practicas/Practica_1_201801210/Header/abrir_en_editor.h
+++ Practicas/Practica_1_201801210/Header/abrir_en_editor.h
@@ -1,12 +1,14 @@
 #ifndef ABRIR_EN_EDITOR_H
 #define ABRIR_EN_EDITOR_H
-
+#include <string>
+using namespace std;
 
 class Abrir_En_Editor
 {
 public:
     Abrir_En_Editor();
     static void crear_EditorA();
+    static void leer_archivo(string direccion);
 };
 
 #endif // ABRIR_EN_EDITOR_H
