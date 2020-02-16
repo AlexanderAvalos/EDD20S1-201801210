9464b5b076377116631828e5a942a51c8c29bdab
AlexanderAvalos <60333367+AlexanderAvalos@users.noreply.github.com>
2020-02-15 23:49:03 -0600

    Create datos_ruta.h

---
+++ Practicas/Practica_1_201801210/Header/datos_ruta.h
@@ -0,0 +1,30 @@
+
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+using namespace std;
+class Rutas{
+private:
+    string nombre;
+    string direccion;
+    Rutas *Siguiente;
+public:
+    string getnombre(){
+        return nombre;
+    }
+    void setnombre(string nombr){
+        nombre=nombr;
+    }
+    string getdireccion(){
+        return direccion;
+    }
+    void setdireccion(string dir){
+        direccion = dir;
+    }
+    Rutas *getsiguiente(){
+        return Siguiente;
+    }
+    void setsiguiente(Rutas *sig){
+        Siguiente=sig;
+    }
+};
