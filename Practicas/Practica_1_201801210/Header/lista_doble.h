c4f04a02375c3cb832682777470162ae2d5879e9
AlexanderAvalos <60333367+AlexanderAvalos@users.noreply.github.com>
2020-02-15 23:50:22 -0600

    Update lista_doble.h

--- Practicas/Practica_1_201801210/Header/lista_doble.h
+++ Practicas/Practica_1_201801210/Header/lista_doble.h
@@ -1,15 +1,63 @@
-#ifndef LISTA_DOBLE_H
-#define LISTA_DOBLE_H
+#include <stdlib.h>
+#include <iostream>
+#include <stdio.h>
+using namespace std;
 
+class Nodo{
 
-class lista_Doble
-{
 public:
-    lista_Doble();
-    static bool vacio();
-    static void mostrar_Izquierda_a_Derecha();
-    static void mostrar_Derecha_a_Izquierda();
-    static void insertar_Nodo(char dato);
-};
+    Caracter *primero = NULL;
+    Caracter *ultimo =NULL;
 
-#endif // LISTA_DOBLE_H
+    bool vacio(){
+        if(primero == NULL){
+           return true;
+        }
+        return false;
+    }
+    void borrar_ultimo(){
+        Caracter *auxB = ultimo->getanterior();
+        auxB->setsiguiente(NULL);
+        ultimo = auxB;
+    }
+     void insertar_Nodo(char dato){
+        Caracter *nuevo = new Caracter();
+        nuevo->setcaracter(dato);
+        if(vacio()== true){
+            primero = nuevo;
+            nuevo->setsiguiente(NULL);
+            nuevo->setanterior(NULL);
+            ultimo = primero;
+        }
+        else{
+            ultimo->setsiguiente(nuevo);
+            nuevo->setsiguiente(NULL);
+            nuevo->setanterior(ultimo);
+            ultimo=nuevo;
+        }
+    }
+    void mostrar_Derecha_a_Izquierda(){
+        Caracter *actual = new Caracter();
+        actual = primero;
+        if(vacio() != true){
+            while (actual != NULL) {
+                cout<< actual->getcaracter() <<"->";
+                actual = actual->getsiguiente();
+            }
+             cout<< "\n";
+        }
+        else{cout <<"lista vacia";}
+    }
+    void mostrar_Izquierda_a_Derecha(){
+        Caracter *actual = new Caracter();
+        actual = ultimo;
+        if(vacio() != true){
+            while (actual != NULL) {
+                cout<<actual->getcaracter()<<"->";
+                actual = actual->getanterior();
+            }
+            cout<< "\n";
+        }
+        else{cout <<"lista vacia";}
+    }
+};
