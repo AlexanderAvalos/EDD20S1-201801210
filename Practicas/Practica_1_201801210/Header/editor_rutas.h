be991ab921f0544d50d586eef6e3c8cf05cda68e
AlexanderAvalos <60333367+AlexanderAvalos@users.noreply.github.com>
2020-02-18 23:20:50 -0600

    Update editor_rutas.h

--- Practicas/Practica_1_201801210/Header/editor_rutas.h
+++ Practicas/Practica_1_201801210/Header/editor_rutas.h
@@ -1,12 +1,12 @@
 #ifndef EDITOR_RUTAS_H
 #define EDITOR_RUTAS_H
-
+#include "lista_circular.h"
 
 class Editor_Rutas
 {
 public:
     Editor_Rutas();
-    static void e_rutas();
+    static void e_rutas(NodoC *lista);
 };
 
 #endif // EDITOR_RUTAS_H
