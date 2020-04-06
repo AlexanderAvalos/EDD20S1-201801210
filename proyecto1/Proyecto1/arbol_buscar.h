9d56aebc155c8fe514f55a84e3fcab0b8e39b634
AlexanderAvalos <60333367+AlexanderAvalos@users.noreply.github.com>
2020-04-05 21:45:38 -0600

    Update arbol_buscar.h

--- proyecto1/Proyecto1/arbol_buscar.h
+++ proyecto1/Proyecto1/arbol_buscar.h
@@ -7,8 +7,8 @@
 class arbol_busqueda{
 public:
 arbol_busqueda(){}
-    void insertarDato(QString nombre){
 
+    void insertarDato(QString nombre){
         insertar(raiz,nombre);
     }
     void graficar(){
@@ -20,13 +20,38 @@ arbol_busqueda(){}
     nodoArbol *buscar(QString valor){
          return buscar(valor, raiz);
      }
+    void inOrden(){
+        salida.open("InOrden.txt");
+        salida << "digraph { rankdir=LR "<<endl;
+        salida<< inOrden(raiz)<<endl;
+        salida<< "}"<<endl;
+        salida.close();
+        qInfo()<<system("dot -Tpng InOrden.txt -o InOrden.png");
+    }
+    void preOrden(){
+        salida.open("preOrden.txt");
+        salida << "digraph { rankdir=LR "<<endl;
+        salida<< preOrden(raiz)<<endl;
+        salida<< "}"<<endl;
+        salida.close();
+        qInfo()<<system("dot -Tpng preOrden.txt -o preOrden.png");
+    }
+    void postOrden(){
+        salida.open("postOrden.txt");
+        salida << "digraph { rankdir=LR "<<endl;
+        salida<< postOrden(raiz)<<endl;
+        salida<< "}"<<endl;
+        salida.close();
+        qInfo()<<system("dot -Tpng postOrden.txt -o postOrden.png");
+    }
 
 private:
     nodoArbol *raiz;
+    nodoArbol *aux,*aux1,*aux2;
     ofstream salida;
     string prueba;
-    void graphviz(nodoArbol *nodo, nodoArbol *padre ){
 
+    void graphviz(nodoArbol *nodo, nodoArbol *padre ){
         if(nodo != nullptr){
             if(padre == nullptr){
                 salida.open("ArbolBinario.txt");
@@ -46,6 +71,55 @@ private:
         }
     }
 
+    string inOrden(nodoArbol *nodo){
+        string sali =+ "";
+        if(nodo->izquierda != nullptr){
+           sali = sali + inOrden(nodo->izquierda);
+           sali=  sali + aux->datos.toStdString() + "->" + nodo->datos.toStdString()+ ";\n";
+           aux = nodo->izquierda;
+        }
+        sali = sali + nodo->datos.toStdString()  +";\n";
+        aux = nodo;
+        if(nodo->derecha != nullptr){
+            sali = sali +aux->datos.toStdString() + "->" + nodo->derecha->datos.toStdString() + ";\n";
+            aux = nodo->derecha;
+            sali = sali + inOrden(nodo->derecha);
+        }
+        return sali;
+    }
+    string preOrden(nodoArbol *nodo){
+        string sali =+ "";
+        sali = sali + nodo->datos.toStdString()  +";\n";
+        aux1 = nodo;
+        if(nodo->izquierda != nullptr){
+           sali=  sali + aux1->datos.toStdString() + "->" + nodo->izquierda->datos.toStdString()+ ";\n";
+           aux1 = nodo->izquierda;
+           sali = sali + preOrden(nodo->izquierda);
+        }
+        if(nodo->derecha != nullptr){
+            sali = sali +aux1->datos.toStdString() + "->" + nodo->derecha->datos.toStdString() + ";\n";
+            sali = sali + preOrden(nodo->derecha);
+            aux1 = nodo->derecha;
+
+        }
+        return sali;
+    }
+    string postOrden(nodoArbol *nodo){
+        string sali =+ "";
+        if(nodo->izquierda != nullptr){
+           aux2 = nodo->izquierda;
+           sali = sali + postOrden(nodo->izquierda);
+        }
+        if(nodo->derecha != nullptr){
+            sali = sali +aux2->datos.toStdString() + "->" + nodo->derecha->datos.toStdString() + ";\n";
+            aux2 = nodo->derecha;
+            sali = sali + postOrden(nodo->derecha);
+            sali = sali +aux2->datos.toStdString() + "->" + nodo->datos.toStdString() + ";\n";
+        }
+        sali = sali + nodo->datos.toStdString()  +";\n";
+        aux2 = nodo;
+        return sali;
+    }
 
 
     void graficar(nodoArbol *nodo){
