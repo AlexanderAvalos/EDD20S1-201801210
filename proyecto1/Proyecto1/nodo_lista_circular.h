040c53a0c7a87260f40ba9652cdc87fa27c8576f
AlexanderAvalos <60333367+AlexanderAvalos@users.noreply.github.com>
2020-04-05 21:54:06 -0600

    Update nodo_lista_circular.h

--- proyecto1/Proyecto1/nodo_lista_circular.h
+++ proyecto1/Proyecto1/nodo_lista_circular.h
@@ -1,9 +1,11 @@
 #ifndef NODO_LISTA_CIRCULAR_H
 #define NODO_LISTA_CIRCULAR_H
-
+#include<QString>
+using namespace std;
 class nodocirular {
+
 public:
-    string palabra;
+    QString palabra;
     nodocirular *Siguiente;
     nodocirular *Anterior;
 };
