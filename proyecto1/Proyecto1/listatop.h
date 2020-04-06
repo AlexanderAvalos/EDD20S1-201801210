6503da1521f47b277ab1f4c639be3a2f024c2bb9
AlexanderAvalos <60333367+AlexanderAvalos@users.noreply.github.com>
2020-04-05 21:52:01 -0600

    Update listatop.h

--- proyecto1/Proyecto1/listatop.h
+++ proyecto1/Proyecto1/listatop.h
@@ -4,8 +4,8 @@
 class listaTop
 {
 private:
-    nodo *primero;
-    nodo *ultimo;
+    nodot *primero;
+    nodot *ultimo;
     ofstream salida;
     bool vacio(){
         if(primero == NULL && ultimo == NULL){
@@ -15,14 +15,19 @@ private:
     }
 public:
     void grafica(){
-        nodo *actual = new nodo();
+        nodot *actual = new nodot();
         actual = primero;
         salida.open("top.txt");
         salida<<"digraph top{  rankdir=LR  "<<endl;
         salida<<" node [shape=rectangle];  "<<endl;
-        while(actual->siguiente != NULL){
-            salida<< actual->puntuacion.getjugadores()<< ","<< actual->puntuacion.getpuntuacion() << "->" <<actual->puntuacion.getjugadores()<< ","<< actual->siguiente->puntuacion.getpuntuacion() << " ;" <<endl;
+        while(actual != NULL){
+            if(actual->siguiente != NULL){
+            salida<<'\"' <<actual->dato.getjugadores().toStdString() << ","<< actual->dato.getpuntuacion() << "\" ->";
             actual = actual->siguiente;
+            }else {
+                salida<<'\"' <<actual->dato.getjugadores().toStdString() << ","<< actual->dato.getpuntuacion() <<"\";";
+                actual = actual->siguiente;
+            }
         }
         salida<<"}"<<endl;
         salida.close();
@@ -30,49 +35,49 @@ public:
     }
 
     void recorrer(){
-        nodo *aux = new nodo();
+        nodot *aux = new nodot();
         aux = primero;
         while(aux != NULL){
-            cout<<aux->puntuacion.getpuntuacion()<<endl;
+            cout<<aux->dato.getpuntuacion()<<endl;
             aux = aux->siguiente;
         }
     }
-    void agregarpunteo(int puntos,string jugador){
-        nodo *nuevo = new nodo();
-            nuevo->puntuacion.setpuntuacion(puntos) = ;
-            nuevo->puntuacion.setjugadores(jugador);
-            if (vacio())
-            {
+    void agregarpunteo(int puntos,QString jugador){
+        nodot *nuevo = new nodot();
+        nuevo->dato.setpuntuacion(puntos);
+        nuevo->dato.setjugadores(jugador);
+        if (vacio())
+        {
+            primero = nuevo;
+            ultimo = primero;
+        }
+        else
+        {
+            if(puntos > primero->dato.getpuntuacion()){
+                nuevo->siguiente = primero;
                 primero = nuevo;
-                ultimo = primero;
             }
-            else
-            {
-                if(puntos > primero->puntuacion.getpuntuacion()){
-                    nuevo->siguiente = primero;
-                    primero = nuevo;
+            else {
+                nodot *aux = primero;
+                nodot *auxb = primero;
+                while (puntos <= aux->dato.getpuntuacion() && aux->siguiente != NULL)
+                {
+                    auxb = aux;
+                    aux = aux->siguiente;
                 }
-                else {
-                    nodo *aux = primero;
-                    nodo *auxb = primero;
-                    while (puntos <= aux->puntuacion.getpuntuacion() && aux->siguiente != NULL)
-                    {
-                        auxb = aux;
-                        aux = aux->siguiente;
-                    }
-                    if (puntos <= aux->puntuacion.getpuntuacion())
-                    { nuevo->siguiente = aux->siguiente;
-                         aux->siguiente = nuevo;
-                    }
-                    else
-                    {
-                        nuevo->siguiente = aux;
-                        auxb->siguiente = nuevo;
-                    }
+                if (puntos <= aux->dato.getpuntuacion())
+                { nuevo->siguiente = aux->siguiente;
+                     aux->siguiente = nuevo;
+                }
+                else
+                {
+                    nuevo->siguiente = aux;
+                    auxb->siguiente = nuevo;
+                }
 
-                }
             }
-}
+        }
+    }
 
 };
 
