c5c1bd1934dd4d516e9d24b36a86615c6f639891
AlexanderAvalos <60333367+AlexanderAvalos@users.noreply.github.com>
2020-04-05 21:49:45 -0600

    Update juego.h

--- proyecto1/Proyecto1/juego.h
+++ proyecto1/Proyecto1/juego.h
@@ -1,13 +1,20 @@
 #ifndef JUEGO_H
 #define JUEGO_H
-
-
+#include <listadoble.h>
+#include "arbol_buscar.h"
 class juego
 {
 public:
     juego();
     static void crearJugador();
     static void elegirJugadores();
+    static void ponerfichas(ListaDoble *lista);
+    static void leerjason();
+    static void menujuego(nodoArbol *jugador);
+    static void insertarpalabra(nodoArbol *jugador);
+    static void pedirficha(nodoArbol *jugador);
+    static void reportes();
+    static int buscarTD(QString letra,QString x, QString y);
 };
 
 #endif // JUEGO_H
